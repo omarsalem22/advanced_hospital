fe8dc388f544036d57d39d444a65917b24fc99ff
omarsalem22 <omarmohamed25112001@gmail.com>
2025-12-11 04:13:29 +0200

    initial commit

---
+++ src/main/java/com/example/hosptal/HosptalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hosptal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosptalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosptalApplication.class, args);
+	}
+
+}
